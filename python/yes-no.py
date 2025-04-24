e6da9ff569b6548909c4b8990a8112f3cad9c0ad
Ashutosh Pradhan <119951421+ashutoshpradhan3@users.noreply.github.com>
2025-04-24 18:50:53 +0530

    Update yes-no.py

--- python/yes-no.py
+++ python/yes-no.py
@@ -1,38 +1,43 @@
-# importing easygui module
-from easygui import *
+import random
 
-# message / information to be displayed on the screen
-message = "Are you a Geek ?"
+# List of questions with correct answers (True/False)
+question_bank = [
+    ("Is Python a programming language?", True),
+    ("Is HTML a programming language?", False),
+    ("Does the Earth have two moons?", False),
+    ("Is 2 a prime number?", True),
+    ("Can elephants fly naturally?", False),
+    ("Is Java platform-independent?", True),
+    ("Is CSS used for designing layout and styles?", True),
+    ("Is 100 greater than 200?", False),
+    ("Does water boil at 100°C at sea level?", True),
+    ("Is '==' used for assignment in Python?", False)
+]
 
-# title of the window
-title = "GfG - EasyGUI"
+# Randomly select 5 questions
+selected_questions = random.sample(question_bank, 5)
 
-# creating a yes no box
-output = ynbox(message, title)
+score = 0
 
+print("Answer the following questions with 'yes' or 'no':\n")
 
-# if user pressed yes
-if output:
-	
-	# message / information to be displayed on the screen
-	message = "Thats Great !!"
+for i, (question, correct_answer) in enumerate(selected_questions, start=1):
+    user_input = input(f"Q{i}: {question} ").strip().lower()
 
-	# title of the window
-	title = "GfG - EasyGUI"
+    # Convert user input to Boolean
+    if user_input in ['yes', 'y']:
+        user_answer = True
+    elif user_input in ['no', 'n']:
+        user_answer = False
+    else:
+        print("Invalid input! Skipping this question.\n")
+        continue
 
-	# creating a message box
-	msg = msgbox(message, title)
+    # Compare with correct answer
+    if user_answer == correct_answer:
+        score += 1
+        print("Correct!\n")
+    else:
+        print("Incorrect.\n")
 
-# if user pressed No
-else:
-	
-	# message / information to be displayed on the screen
-	message = "You should become a Geek, go to GeeksforGeeks to become one"
-
-	# title of the window
-	title = "GfG - EasyGUI"
-
-	# creating a message box
-	msg = msgbox(message, title)
-	
-	
+print(f"Your final score is: {score}/5")
